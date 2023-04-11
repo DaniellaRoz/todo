67fab35b63c1f52eb850ea41fec0b0f5ba16f583
DaniellaRoz <114352757+DaniellaRoz@users.noreply.github.com>
2023-04-11 14:12:06 +0200

    get basic structure of website

---
+++ src/containers/TodoContainer.jsx
@@ -0,0 +1,3 @@
+export default function TodoContainer() {
+    return <p>TodoContainer</p>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import FormContainer from "./containers/FormContainer";
+import TodoContainer from "./containers/TodoContainer"; 
+import "./index.scss"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("left")).render(
+    <React.StrictMode>
+        <FormContainer />
+    </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById("right")).render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode> 
 )
